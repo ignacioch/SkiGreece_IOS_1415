3f70429e8ab2773de2e8338713470e1a47aa1266
IgnacioCh <ign_ch@hotmail.com>
2014-12-19 13:10:12 +0000

    removing comments

--- ski greece/ViewControllers/CommunityControllers/Home/VTHomeViewController.m
+++ ski greece/ViewControllers/CommunityControllers/Home/VTHomeViewController.m
@@ -114,16 +114,6 @@ typedef enum {
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 
 
 - (IBAction)backButtonAction:(id)sender {
